23265784c7c92703083dffe9581c9a4a0e90a086
yangzhirui <154966231@qq.com>
2020-02-25 18:42:15 +0800

    union find

---
+++ src/main/java/com/datastructure/UnionFind/UnionFind.java
@@ -0,0 +1,31 @@
+package com.datastructure.UnionFind;
+
+/**
+ * 并查集
+ */
+public interface UnionFind {
+
+    /**
+     * i两个元素是否能够相连
+     *
+     * @param p
+     * @param q
+     * @return boolean
+     */
+    boolean isConnected(int p, int q);
+
+    /**
+     * 链接元素
+     *
+     * @param p
+     * @param q
+     */
+    void unionElement(int p, int q);
+
+    /**
+     * 获取并查集元素数量
+     *
+     * @return 数量
+     */
+    int getSize();
+}
